638fbbbad6ad2cf306c254cd1a55d76e22527319
bestamidu <ynhu22887@gmail.com>
2024-12-19 21:06:53 +0700

    3

---
+++ bt3 ss6.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	int matKhau=12345,nhapMatkhau;
+	printf("nhapmk");
+	scanf("%d",&nhapMatkhau);
+	 if(nhapMatkhau==matKhau){
+	 	printf("nhap dung ");}
+	else {
+		printf("nhap sai r ");
+	}
+	return 0;
+	 }
